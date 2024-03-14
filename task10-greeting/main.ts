47b791bf3b89b2f4f73d41454e83e9afe971deae
Your Name <marjan.ahmed08@yahoo.com>
2024-03-14 20:06:54 +0500

    first commit

---
+++ task10-greeting/main.ts
@@ -0,0 +1,9 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names: string[] = ['Ahmed','Abdullah','Yousuf','Abdul Samad','Shabeeb'];
+
+// Print each person's name with message by accessing each element in the list one at a time
+for (let i = 0; i < names.length; i++) {
+    
+    console.log(`${names[i]}, would you like to drink some water?`);
+}
