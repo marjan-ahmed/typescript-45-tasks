44a765e6d6fd5f6484bb7e76d6969ec128325e77
Marjan-ahmed <159646510+marjan-ahmed@users.noreply.github.com>
2024-03-14 18:21:20 +0500

    Delete task10-greeting directory

--- task10-greeting/main.ts
+++
@@ -1,7 +0,0 @@
-// Define the array of names
-let names: string[] = ["Shabeeb", "Abdullah", "Yousuf", "Ahmed", "Khuzaima"];
-
-// Print a personalized message to each person
-for (let i = 0; i < names.length; i++) {
-    console.log(`Greetings, ${names[i]}! Would you like to drink some water`);
-}
