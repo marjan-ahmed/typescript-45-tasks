e19f3654c3a6d07942593ab03431c4e5db205e27
Your Name <marjan.ahmed08@yahoo.com>
2024-03-14 20:32:37 +0500

    first commit

---
+++ task11-transport-array/main.ts
@@ -0,0 +1,7 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+const transports_arr: string[] = ['Honda','Toyota','Suzuki','Nissan','Jeep', 'BMW','Tesla','ferrari'];
+
+//print the list with array index number (starts from 0)
+
+console.log(`In future, I will gift ${transports_arr[7]} to my father`);
